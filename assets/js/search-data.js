186ad1a86ba55e0d545f1da6076c3403fb779b1f
DanR001 <daniel.roberts1997@gmail.com>
2025-10-23 19:48:54 +0000

    Deploying to gh-pages from @ DanR001/danr001.github.io@b0cbab84f148030693825dffe7b087e6be99f99c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64.%6D.%72%6F%62%65%72%74%73@%73%6F%74%6F%6E.%61%63.%75%6B", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/DanR001", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/2904622", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/daniel-roberts-astro", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0009-7662-0445", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +543,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=ZoAVvJ0AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
